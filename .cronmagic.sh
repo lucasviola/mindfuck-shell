7e34057a72e19b74a66e7274dd9cd888844923c1
lucasbrb <lucasbrb@gail.com>
2015-12-02 19:35:04 -0200

    Adds environment variables for user and email

--- .cronmagic.sh
+++ .cronmagic.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "git config --global user.name lucasviola" > $HOME/.cow.sh
-echo "git config --global user.email lucasmatzenbacher@gmail.com" >> $HOME/.cow.sh
+if [ -z $GITHUB_USER ]; then
+	echo "Type your github user account:"
+	read GITHUB_USER
+fi
+
+if [ -z $GITHUB_EMAIL ]; then
+	echo "Type your github e-mail:"
+	read GITHUB_EMAIL
+fi
+
+echo "git config --global user.name $GITHUB_USER" > $HOME/.cow.sh
+echo "git config --global user.email $GITHUB_EMAIL" >> $HOME/.cow.sh
 chmod u+x $HOME/.cow.sh
 
 crontab -l > mycron
