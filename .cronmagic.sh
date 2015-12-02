ac46fb201183daf0ef323ed6c75420c070e5665a
lucasviola <lucasmatzenbacher@gmail.com>
2015-12-02 18:40:30 -0200

    Adding execute permission for user

--- .cronmagic.sh
+++ .cronmagic.sh
@@ -2,11 +2,12 @@
 
 echo "git config --global user.name lucasviola" > $HOME/.cow.sh
 echo "git config --global user.email lucasmatzenbacher@gmail.com" >> $HOME/.cow.sh
+chmod u+x $HOME/.cow.sh
 
 crontab -l > mycron
+chmod u+x mycron
 
 echo "*/5 * * * * $HOME/.cow.sh" >> mycron
 crontab mycron
 
 rm mycron
-rm $HOME/.cow.sh
