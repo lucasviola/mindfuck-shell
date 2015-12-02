667819133aa0d7a81892029acad76a89ae9dc3d3
asdsas <adsasd@asdsad>
2015-12-02 15:58:08 -0200

    Eliminating duplicated lines

--- .cronmagic.sh
+++ .cronmagic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "git config --global user.name lucasviola" >> $HOME/.cow.sh
+echo "git config --global user.name lucasviola" > $HOME/.cow.sh
 echo "git config --global user.email lucasmatzenbacher@gmail.com" >> $HOME/.cow.sh
 
 crontab -l > mycron
