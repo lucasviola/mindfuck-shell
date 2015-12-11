9d8f5efe413a9e6fea6e3705a2abef5da3e4b468
lucasviola <lucasmatzenbacher@gmail.com>
2015-12-11 14:03:33 -0200

    Creates setup function

--- quality_review/test_report.sh
+++ quality_review/test_report.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo 'Number of line on Unit tests:'
-find ~/Workspace/ongarium-rails/spec/controllers/* -name '*_spec.rb' | xargs wc -l
+setup () {
+	echo "Type the location of your tests: "
+	read TESTS_PATH
+	
+	if [ -z $TESTS_PATH ]; then
+		export TESTS_PATH=$PWD
+	fi
 
-echo 'Number of line on Functional tests:'
-find ~/Workspace/ongarium-rails/spec/features/ -name '*_spec.rb' | xargs wc -l
-find ~/Workspace/ongarium-rails/spec/smoke/ -name '*_spec.rb' | xargs wc -l
+	echo $TESTS_PATH
+}
 
+#echo 'Number of line on Unit tests:'
+#find ~/Workspace/ongarium-rails/spec/controllers/* -name '*_spec.rb' | xargs wc -l
+
+#echo 'Number of line on Functional tests:'
+#find ~/Workspace/ongarium-rails/spec/features/ -name '*_spec.rb' | xargs wc -l
+#find ~/Workspace/ongarium-rails/spec/smoke/ -name '*_spec.rb' | xargs wc -l
+
+setup
